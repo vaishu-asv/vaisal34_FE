73a49b96e59cafebec89fc2ee055be9f290fc63a
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 11:51:35 +0530

    Initial commit

---
+++ src/app/modules/create-what-if-analysis/create-what-if-analysis.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-what-if-analysis',
+  templateUrl: './create-what-if-analysis.component.html',
+  styleUrls: ['./create-what-if-analysis.component.scss']
+})
+export class CreateWhatIfAnalysisComponent {
+
+}
