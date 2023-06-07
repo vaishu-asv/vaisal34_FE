4413df419d664ab078fde8b8f7d7d771ec4aa855
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 11:51:32 +0530

    Initial commit

---
+++ src/app/share/createwhatif/createwhatif.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createwhatif',
+  templateUrl: './createwhatif.component.html',
+  styleUrls: ['./createwhatif.component.scss']
+})
+export class CreatewhatifComponent {
+
+}
