304ee9124f552b5d7ef6178e3bc4dff93bea4aa8
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 11:51:47 +0530

    Initial commit

---
+++ src/app/core/actual-data-dash-board/actual-data-dash-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actual-data-dash-board',
+  templateUrl: './actual-data-dash-board.component.html',
+  styleUrls: ['./actual-data-dash-board.component.scss']
+})
+export class ActualDataDashBoardComponent {
+
+}
