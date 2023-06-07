70c1c9f3bfb5cdebe2e6a801211d239be04ad305
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 11:51:34 +0530

    Initial commit

---
+++ src/app/modules/create-base/create-base.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-base',
+  templateUrl: './create-base.component.html',
+  styleUrls: ['./create-base.component.scss']
+})
+export class CreateBaseComponent {
+
+}
