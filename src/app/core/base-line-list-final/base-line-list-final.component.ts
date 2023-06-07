1584e59d0dac5d0344c9bdf1bc3e5436026d8b27
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 11:51:40 +0530

    Initial commit

---
+++ src/app/core/base-line-list-final/base-line-list-final.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-line-list-final',
+  templateUrl: './base-line-list-final.component.html',
+  styleUrls: ['./base-line-list-final.component.scss']
+})
+export class BaseLineListFinalComponent {
+
+}
