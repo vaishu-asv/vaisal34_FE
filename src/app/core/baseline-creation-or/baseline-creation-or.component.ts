8fbdd9e643db919a9ff0680ad583831059baf8ae
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 11:51:42 +0530

    Initial commit

---
+++ src/app/core/baseline-creation-or/baseline-creation-or.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baseline-creation-or',
+  templateUrl: './baseline-creation-or.component.html',
+  styleUrls: ['./baseline-creation-or.component.scss']
+})
+export class BaselineCreationOrComponent {
+
+}
